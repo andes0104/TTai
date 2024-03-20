a89db3f0e5c4b6e0ccac0d68310fba26207bdb32
DESKTOP-UF80FUP\YI-AN <andes0104@gmail.com>
2024-03-20 22:58:18 +0800

    重新用while迴圈寫cap.isOpened()

--- app/utilities/image_capture.py
+++ app/utilities/image_capture.py
@@ -2,32 +2,31 @@ import cv2
 import os
 import time
 
+
 def video_split(video_path, save_path):
-    vc = cv2.VideoCapture(video_path)
-    c = 0
-    if vc.isOpened():
-        rval, frame = vc.read()
-        cv2.imshow('frame', frame)
-    else:
-        rval = False
-    while rval:
-        rval, frame = vc.read()
-        if c %30 == 0:
-            cv2.imwrite(f"save_path/{str(c//30)}.jpg", frame)
-            cv2.waitKey(1)
-            print(f"save_path/{str(c//30)}.jpg")
-        c += 1
-    vc.release()
+    cap = cv2.VideoCapture(video_path)
+    frame_num = 0
+    while cap.isOpened():
+        rval, frame = cap.read()
+        if not rval:
+            break
+        if frame_num % 30 == 0:
+            cv2.imwrite(f"{save_path}/{str(frame_num//30)}.jpg", frame)
+            cv2.waitKey(100)
+            print(f"{save_path}/{str(frame_num//30)}.jpg")
+            frame_num += 1
+        cap.release()
+
 
 DATA_DIR = "./test_video/forehand"
 SAVE_DIR = "./test_pre_train_image"
 
 start_time = time.time()
 
-for video in os.listdir (DATA_DIR):
+for video in os.listdir(DATA_DIR):
     video_path = f"{DATA_DIR}/{video}"
     video_name = os.path.splitext(video)[0]  # 獲取不含副檔名的影片名稱
-    save_path = f"{SAVE_DIR}/{video_name}"  # 使用影片名稱建立儲存路徑
+    save_path = f"{SAVE_DIR}/forehand1/{video_name}"  # 使用影片名稱建立儲存路徑
     os.makedirs(save_path, exist_ok=True)
     video_split(video_path, save_path)
 
