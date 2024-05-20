7fe4526d664432d32f3f619f6863015d1f72b0eb
DESKTOP-UF80FUP\YI-AN <andes0104@gmail.com>
2024-05-20 23:26:09 +0800

    刪除LSTM架構

--- app/utilities/load_lstm_model.py
+++
@@ -1,9 +0,0 @@
-from app.utilities.setting import Env
-from app.utilities.lstm_constructure import model_constructure
-
-LSTM_MODEL_FILE = Env().LSTM_MODEL_FILE
-
-def load_lstm_model():
-    model = model_constructure()
-    model.load_weights(LSTM_MODEL_FILE)
-    return model
